64c3caba7fa76d3f94de51682b465778727aaedb
nimgnas <sangminyn@gmail.com>
2022-10-14 17:56:02 +0900

    Detail 페이지 view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Detail from "./Detail";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Detail />
+    </div>
+  );
 }
 
 export default App;
